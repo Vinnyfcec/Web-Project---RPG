56cfbfa7e4dc02ee2a25e6b1f68154747f5f9ffc
Vinnyfcec <136275978+Vinnyfcec@users.noreply.github.com>
2025-10-31 17:33:42 -0300

    inicializando

---
+++ app/src/config/db.js
@@ -0,0 +1,11 @@
+require('dotenv').config();
+const mysql = require('mysql2'); 
+
+const connection = mysql.createPool({
+    host: process.env.db_host || 'localhost',
+    user: process.env.db_user || 'root',
+    password: process.env.db_password || '',
+    database: process.env.db_name || 'rpg_db',
+});
+
+module.exports = connection.promise();
