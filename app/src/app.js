47f4e2d15a0e58c47620044cac08034d70af632d
Vinnyfcec <viniciusfcec@gmail.com>
2025-11-20 19:56:11 -0300

    novas rotas

--- app/src/app.js
+++ app/src/app.js
@@ -21,12 +21,12 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(express.static(path.join(__dirname, 'public')));
 
-//const userRoutes = require('./routes/userRoutes');
-//const saveRoutes = require('./routes/saveRoutes');
-//const lojaRoutes = require('./routes/lojaRoutes');
-//app.use('/', userRoutes);
-//app.use('/', saveRoutes);
-//app.use('/', lojaRoutes);
+const userRoutes = require('./routes/userRoutes');
+const saveRoutes = require('./routes/saveRoutes');
+const lojaRoutes = require('./routes/lojaRoutes');
+app.use('/', userRoutes);
+app.use('/', saveRoutes);
+app.use('/', lojaRoutes);
 
 app.get('/', (req, res) => {
   res.render('home', { titulo: 'pagina inuail rpg' }); //é render k7, n sf
