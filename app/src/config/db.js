6e5294439dd2050dd47fa9b4dba0fe46db9c1d6a
Vinnyfcec <viniciusfcec@gmail.com>
2025-11-20 20:14:31 -0300

    arrumando

--- app/src/config/db.js
+++ app/src/config/db.js
@@ -4,7 +4,7 @@ const mysql = require('mysql2');
 const connection = mysql.createPool({
     host: process.env.db_host || 'localhost',
     user: process.env.db_user || 'root',
-    password: process.env.db_password || '',
+    password: process.env.db_password || 'aluno123',
     database: process.env.db_name || 'rpg_db',
 });
 
--- app/src/database/setup.sql
+++ app/src/database/setup.sql
@@ -14,6 +14,8 @@ CREATE TABLE IF NOT EXISTS `saves` (
   `usuario_id` INT NOT NULL,
   `nome_save` VARCHAR(100) NOT NULL,
   `dinheiro` INT NOT NULL DEFAULT 10,
+  `nivel` INT NOT NULL DEFAULT 1,
+  `itens_adquiridos` INT NOT NULL DEFAULT 0,
   `data_ultima_atualizacao` TIMESTAMP DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
   PRIMARY KEY (`id`),
   UNIQUE KEY `uk_usuario_nome` (`usuario_id`, `nome_save`),
@@ -23,3 +25,22 @@ CREATE TABLE IF NOT EXISTS `saves` (
     ON DELETE CASCADE
     ON UPDATE CASCADE
 ) ENGINE = InnoDB;
+
+CREATE TABLE IF NOT EXISTS `inventario` (
+  'id' INT NOT NULL AUTO_INCREMENT,
+  'save_id' INT NOT NULL,
+  'itenm_base_id' INT NOT NULL,
+  'quantidade' INT NOT NULL DEFAULT 1,
+  'equipado' BOOLEAN NOT NULL DEFAULT FALSE,
+  PRIMARY KEY ('id'),
+  CONSTRAINT 'fk_inventario_saves'
+    FOREIGN KEY ('save_id')
+    REFERENCES 'saves' ('id')
+    ON DELETE CASCADE
+    ON UPDATE CASCADE,
+  CONSTRAINT 'fk_inventario_itens_base'
+    FOREIGN KEY ('itenm_base_id')
+    REFERENCES 'itens_base' ('id')
+    ON DELETE RESTRICT
+    ON UPDATE CASCADE
+) ENGINE = InnoDB; 
